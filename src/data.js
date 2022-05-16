7f0953be16e46fb84a9c2cecad84c35158742422
Delbert <55643100+del8527@users.noreply.github.com>
2022-05-16 04:01:43 -0400

    Update data.js
    added del bot

--- src/data.js
+++ src/data.js
@@ -2,16 +2,21 @@
 export const products = [
   {
     id: 1,
+    img: "blob:https://imgur.com/a2803525-87ba-4118-a983-7904957c72e5",
+    link: "https://github.com/del8527/del-bot",
+  },
+  {
+    id: 2,
     img: "https://i.imgur.com/vcM1m54.png",
     link: "http://typing.delbertluo.com/",
   },
   {
-    id: 2,
+    id: 3,
     img: "https://i.imgur.com/2TETUGf.jpg",
     link: "https://del8527.github.io/",
   },
   {
-    id: 3,
+    id: 4,
     img: "https://i.imgur.com/U4iB1h1.jpg",
     link: "https://github.com/del8527/Digital-Clock-in-C",
   },
