e9efc2651604f01ae22be46b71538686de2830f4
Delbert <55643100+del8527@users.noreply.github.com>
2022-07-01 00:19:55 -0400

    update data

--- src/data.js
+++ src/data.js
@@ -2,21 +2,31 @@
 export const products = [
   {
     id: 1,
+    img: "https://i.imgur.com/po00Llb.png",
+    link: "https://colab.research.google.com/drive/1DnyL-g3GiQQfkA77WT_Kbfq_3XEMaFs7?usp=sharing",
+  },
+  {
+    id: 2,
     img: "https://i.imgur.com/QOQFjeZ.png",
     link: "https://github.com/del8527/del-bot",
   },
   {
-    id: 2,
+    id: 3,
+    img: "https://i.imgur.com/fRYFX1E.png",
+    link: "http://sort.delbertluo.com/",
+  },
+  {
+    id: 4,
     img: "https://i.imgur.com/vcM1m54.png",
     link: "http://typing.delbertluo.com/",
   },
   {
-    id: 3,
+    id: 5,
     img: "https://i.imgur.com/2TETUGf.jpg",
     link: "https://del8527.github.io/",
   },
   {
-    id: 4,
+    id: 6,
     img: "https://i.imgur.com/U4iB1h1.jpg",
     link: "https://github.com/del8527/Digital-Clock-in-C",
   },
